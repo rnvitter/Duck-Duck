6b6aec12fa9c489aafa2afdf8b00e19e33d5b8fe
Ryan Vitter <ryanvitter@gmail.com>
2021-04-01 17:31:50 -0400

    move firebase config to its own file and wait for auth state to mount app

---
+++ client/src/firebase.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+
+const usersCollection = db.collection('users');
+const pollsCollection = db.collection('polls');
+
+export {
+  db,
+  auth,
+  usersCollection,
+  pollsCollection
+}
--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,10 @@
 import { createApp } from 'vue';
 import { IonicVue } from '@ionic/vue';
-import firebase from 'firebase';
 
 import App from './App.vue';
 import router from './router';
 import { store } from './store';
-
+import { auth } from './firebase'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -26,29 +25,19 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID,
-  measurementId: process.env.VUE_APP_MEASUREMENT_ID
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+let app
+auth.onAuthStateChanged(user => {
+  if (!app) {
+    const app = createApp(App)
+      .use(IonicVue)
+      .use(router)
+      .use(store);
+    router.isReady().then(() => {
+      app.mount('#app');
+    });
+  }
 
-export const db = firebase.firestore();
-export const auth = firebase.auth();
-
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router)
-  .use(store);
-
-router.isReady().then(() => {
-  app.mount('#app');
-});
+  if (user) {
+    store.dispatch('fetchUserProfile', user)
+  }
+})
