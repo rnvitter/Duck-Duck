2999f1a01edce992efb58081a045c52c158cc2a7
Ryan Vitter <ryanvitter@gmail.com>
2021-04-02 15:12:34 -0400

    Remove participants component and add in author instead

--- src/components/PollItem.vue
+++ src/components/PollItem.vue
@@ -1,12 +1,21 @@
 <template>
   <ion-card
+    v-if="poll"
     :router-link="`/polls/${poll.id}`"
     style="border-radius: 25px; margin: 20px 0;">
     <ion-card-header>
       <ion-card-title style="font-size: 18px;">{{ poll.name }}</ion-card-title>
     </ion-card-header>
     <ion-card-content style="display: flex; align-items: center; justify-content: space-between;">
-      <participants></participants>
+      <div style="display: flex; align-items: center;">
+        <ion-avatar style="height: 40px; width: 40px;">
+          <img :src="poll.author.image">
+        </ion-avatar>
+        <div style="margin-left: 5px;">
+          <div style="font-size: 13px;">Created by</div>
+          <div style="font-weight: 700;">{{ poll.author.username }}</div>
+        </div>
+      </div>
       <div style="font-size: 13px; text-align: end;">
         <div>Voted</div>
         <div>2 days left</div>
@@ -17,6 +26,7 @@
 
 <script>
 import {
+  IonAvatar,
   IonCard,
   IonCardContent,
   IonCardTitle,
@@ -24,16 +34,14 @@ import {
 } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
-import { Participants } from '@/components'
-
 export default defineComponent({
   name: 'PollItem',
   components: {
+    IonAvatar,
     IonCard,
     IonCardContent,
     IonCardTitle,
-    IonCardHeader,
-    Participants
+    IonCardHeader
   },
   props: {
     poll: {
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import Layout from './Layout'
-import Participants from './Participants'
 import PollItem from './PollItem'
 import PollForm from './PollForm'
 
 export {
   Layout,
-  Participants,
   PollItem,
   PollForm
 }
