962b65aa4b0c5934903c71a51eebc8eff987b8d1
Ryan Vitter <ryanvitter@gmail.com>
2021-04-05 11:13:00 -0400

    remove test data

--- src/modules/polls.js
+++ src/modules/polls.js
@@ -3,23 +3,6 @@ import { auth } from '@/firebase';
 
 const store = {
   state: {
-    // polls: [
-    //   {
-    //     id: 1,
-    //     name: 'Movie Night 3/31/30',
-    //     items: ['Judas and the Black Messiah', 'Star Wars: A New Hope', 'Soul', 'Wall-E', 'Goodwill Hunting']
-    //   },
-    //   {
-    //     id: 2,
-    //     name: 'Next Binge Watch',
-    //     items: ['Fargo', 'Parks & Rec', 'What We Do In The Shadows', 'The Mandolorian']
-    //   },
-    //   {
-    //     id: 3,
-    //     name: 'Next Book For Book Club',
-    //     items: ['March', 'The Human Cosmos', 'It\'s What I Do', 'Moon Walking With Einstein']
-    //   }
-    // ]
     polls: [],
     votes: []
   },
