24871f7e325560459990baa2bdb29889845d2f71
Ryan Vitter <ryanvitter@gmail.com>
2021-04-05 11:00:14 -0400

    add not found view

---
+++ src/views/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+  <ion-content :fullscreen="true">
+    <ion-card style="box-shadow: none; background: transparent; top: 50%; transform: translateY(-55%);">
+      <ion-card-header class="ion-text-center">
+        <ion-card-subtitle>Error 404</ion-card-subtitle>
+        <ion-card-title class="mb-lg">This is not the page you are looking for</ion-card-title>
+        <img src="assets/404.png" style="width: 200px; height: auto;">
+      </ion-card-header>
+    </ion-card>
+  </ion-content>
+</template>
+
+<script>
+import {
+ IonCard,
+ IonCardHeader,
+ IonCardTitle,
+ IonCardSubtitle,
+ IonContent
+} from '@ionic/vue';
+
+export default {
+  name: 'NotFound',
+  components: {
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonContent
+  }
+}
+</script>
